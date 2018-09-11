bd9500de9072d890d5cc043bd0418c6df526312d
Pekhov <alexey@pekhov.com>
2018-09-11 16:10:32 +0300

    Исправил

--- Основы Ruby. Часть 1/Квадратное_уравнение.rb
+++ Основы Ruby. Часть 1/Квадратное_уравнение.rb
@@ -1,15 +1,15 @@
 print "Введите 3 коэффициента через запятую "
-a, b, c = gets.chomp.split(',').collect!(&:to_i)
-dis = b**2 - 4*a*c
+a, b, c = gets.chomp.split(',').map!(&:to_i)
+dis = b ** 2 - 4 * a * c
+dis_sqrt = Math.sqrt(dis) if dis > 0
 puts "Дискриминант равен #{dis}"
-case
-  when dis.positive?
-    x1 = (-b+Math.sqrt(dis))/2*a
-    x2 = (-b-Math.sqrt(dis))/2*a
-    puts "Корни уравнения: x1=#{x1}, x2=#{x2}"
-  when dis.zero?
-    x1 = (-b+Math.sqrt(dis))/2*a
-    puts "Корень уравнения: x1=#{x1}"
-  when dis.negative?
+if dis.positive?
+    x1 = (-b + dis_sqrt) / (2 * a)
+    x2 = (-b - dis_sqrt) / (2 * a)
+    puts "Корни уравнения: x1 = #{x1}, x2=#{x2}"
+elsif dis.zero?
+    x1 = -b / (2 * a)
+    puts "Корень уравнения: x1 = #{x1}"
+else
     puts "Корней нет"
 end
