0c6621d8fe88d71af93a31c2794dd4a6be83bdee
Pekhov <alexey@pekhov.com>
2018-09-11 16:46:12 +0300

    Исправил опечатку

--- Основы Ruby. Часть 1/Площадь_треугольника.rb
+++ Основы Ruby. Часть 1/Площадь_треугольника.rb
@@ -1,4 +1,4 @@
 print "Введите высоту и основание треугольника через запятую "
 height, base = gets.chomp.split(',').map(&:to_i)
-area = 0.5 / 2 * height * base
+area = 0.5 * height * base
 puts "Площадь треугольника: #{area.to_i}"
