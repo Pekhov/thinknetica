ba00ca30e33e01b4ef34bcc9817f0a2fd4c631d8
Pekhov <alexey@pekhov.com>
2018-09-12 08:24:26 +0300

    Исправил замечания

--- Основы Ruby. Часть 1/Прямоугольный_треугольник.rb
+++ Основы Ruby. Часть 1/Прямоугольный_треугольник.rb
@@ -1,14 +1,9 @@
 print "Введите 3 стороны треугольника через запятую "
 all_side = gets.chomp.split(',').map(&:to_i)
-temp_array = all_side.clone
-hypotenuse = all_side.max # Нашли гипотенузу
-all_side.each_with_index do |side, index|
-  if side == hypotenuse
-    all_side.delete_at(index) # Удаляем первую попавшуюся сторону, которая является гипотенузой и выходим из цикла. В массиве останутся две другие стороны.
-    break
-  end
-end
-if hypotenuse**2 == all_side.first**2 + all_side.last**2
+all_side.sort!
+check_isosceles = all_side[2]**2 == all_side[0]**2 + all_side[1]**2 ? true: false
+if check_isosceles && all_side[0] == all_side[1]
+  puts "Треугольник прямоугольный и равнобедренный"
+elsif check_isosceles
   puts "Треугольник прямоугольный"
-  puts "Треугольник равнобедренный" if temp_array.uniq.size == 2
 end
