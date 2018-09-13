d1fb8866cffb1730e2d3224f16c898b5f8c85c44
Pekhov <alexey@pekhov.com>
2018-09-13 09:28:42 +0300

    Убрал тернарный оператор

--- Основы Ruby. Часть 1/Прямоугольный_треугольник.rb
+++ Основы Ruby. Часть 1/Прямоугольный_треугольник.rb
@@ -1,9 +1,9 @@
 print "Введите 3 стороны треугольника через запятую "
 all_side = gets.chomp.split(',').map(&:to_i)
 all_side.sort!
-check_isosceles = all_side[2]**2 == all_side[0]**2 + all_side[1]**2 ? true: false
-if check_isosceles && all_side[0] == all_side[1]
+right_triangle = all_side[2]**2 == all_side[0]**2 + all_side[1]**2
+if right_triangle && all_side[0] == all_side[1]
   puts "Треугольник прямоугольный и равнобедренный"
-elsif check_isosceles
+elsif right_triangle
   puts "Треугольник прямоугольный"
 end
