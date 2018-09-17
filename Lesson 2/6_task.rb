8a751be0c0fa522e66f506dd10548c0832d0b656
Pekhov <alexey@pekhov.com>
2018-09-17 14:06:51 +0300

    Исправил замечания

--- Lesson 2/6_task.rb
+++ Lesson 2/6_task.rb
@@ -7,13 +7,13 @@ loop do
   price = gets.chomp
   print "Введите количество товара "
   amount = gets.chomp
-  purchases[product_name] = {price => amount}
+  purchases[product_name] = { price: price, amount: amount }
 end
 product_sum = 0
 puts "Вы купили: " + purchases.to_s
 purchases.each_pair do |product, hash|
-    item_sum = (hash.keys.join.to_f * hash.values.join.to_f).round(3)
-    puts "Вы купили товар #{product} на сумму #{item_sum}"
+    item_sum = (hash[:price].to_f * hash[:amount].to_f).round(3)
+    puts "Вы купили товар #{ product } на сумму #{ item_sum }"
     product_sum += item_sum
 end
-puts "Всего вы купили товаров на сумму: #{product_sum}"
+puts "Всего вы купили товаров на сумму: #{ product_sum }"
