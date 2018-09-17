316c7678f16c2a7d7d43ed011da5a734d3d3e435
Pekhov <alexey@pekhov.com>
2018-09-17 12:26:19 +0300

    Исправил замечания

--- Lesson 2/5_task.rb
+++ Lesson 2/5_task.rb
@@ -1,6 +1,7 @@
 print "Введите дату в формате ДД.ММ.ГГГГ "
 day, month, year = gets.chomp.split('.').map!(&:to_i)
-intercalary_year = (year % 4 == 0) || (year % 400 == 0) || (year % 100 != 0)
-months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-months[2] = intercalary_year ? 29 : 28
-puts "Текущий день с начала года является: " + (months.values_at(1..month - 1).inject(:+) + day).to_s
+intercalary_year = year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+months[1] = 29 if intercalary_year
+current_day = month == 1 ? day : months.take(month - 1).inject(:+) + day
+puts "Текущий день с начала года является: #{current_day}"
